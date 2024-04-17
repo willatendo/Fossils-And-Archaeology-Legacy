425a0b3df72ecca75814e9f208b1da4afa8a9af9
Willatendo <williamjswindler@gmail.com>
2024-04-17 17:10:59 -0600

    Fix

--- src/main/java/willatendo/fossilslegacy/server/item/ToothDaggerItem.java
+++ src/main/java/willatendo/fossilslegacy/server/item/ToothDaggerItem.java
@@ -5,7 +5,12 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.MushroomCow;
+import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.entity.animal.SnowGolem;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
@@ -20,6 +25,38 @@ import net.minecraft.world.level.gameevent.GameEvent;
 public class ToothDaggerItem extends SwordItem {
 	public ToothDaggerItem(Tier tier, int attackDamage, float attackSpeed, Properties properties) {
 		super(tier, attackDamage, attackSpeed, properties);
+	}
+
+	@Override
+	public InteractionResult interactLivingEntity(ItemStack itemStack, Player player, LivingEntity livingEntity, InteractionHand interactionHand) {
+		if (livingEntity.level().isClientSide) {
+			return InteractionResult.PASS;
+		}
+		if (livingEntity instanceof Sheep sheep) {
+			if (sheep.readyForShearing()) {
+				sheep.shear(SoundSource.PLAYERS);
+				sheep.gameEvent(GameEvent.SHEAR, player);
+				itemStack.hurtAndBreak(1, player, user -> user.broadcastBreakEvent(interactionHand));
+				return InteractionResult.SUCCESS;
+			}
+		}
+		if (livingEntity instanceof MushroomCow mushroomCow) {
+			if (mushroomCow.readyForShearing()) {
+				mushroomCow.shear(SoundSource.PLAYERS);
+				mushroomCow.gameEvent(GameEvent.SHEAR, player);
+				itemStack.hurtAndBreak(1, player, user -> user.broadcastBreakEvent(interactionHand));
+				return InteractionResult.SUCCESS;
+			}
+		}
+		if (livingEntity instanceof SnowGolem snowGolem) {
+			if (snowGolem.readyForShearing()) {
+				snowGolem.shear(SoundSource.PLAYERS);
+				snowGolem.gameEvent(GameEvent.SHEAR, player);
+				itemStack.hurtAndBreak(1, player, user -> user.broadcastBreakEvent(interactionHand));
+				return InteractionResult.SUCCESS;
+			}
+		}
+		return InteractionResult.PASS;
 	}
 
 	@Override
